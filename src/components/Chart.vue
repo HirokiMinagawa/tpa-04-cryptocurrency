d4e5fb24963fd8838cbf5746e2ef7c478496a070
hiroki minagawa <hiroki6805@gmail.com>
2019-02-07 14:07:16 +0900

    Chart.js の実装

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,9 +1,24 @@
-<template>
-  <div>TODO: Chart</div>
-</template>
-
 <script>
+import { Line, mixins } from 'vue-chartjs';
+const { reactiveProp } = mixins;
+
 export default {
-  name: 'Chart',
+  extends: Line,
+  mixins: [reactiveProp],
+  props: {
+    chartData: null,
+    options: null,
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+  watch: {
+    chartData: function() {
+      this.renderChart(this.chartData, this.options);
+    },
+    options: function() {
+      this.renderChart(this.chartData, this.options);
+    },
+  },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   // https://cli.vuejs.org/config/#devserver
   devServer: {
-    proxy: 'http://localhost:3000'
+    proxy: 'http://localhost:3000',
+    host: 'localhost'
   }
 };
